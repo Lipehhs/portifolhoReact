16865a853f74f4a304baddf63a6e382f15976f57
Felipe Alcantra <lipao01017@gmail.com>
2025-11-20 22:19:09 -0300

    iniciando projeto de portifolho

---
+++ src/header.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import './header.css'
+
+function header(){
+return(
+    <>
+    <nav>
+        <ul>
+            <li>Sobre</li>
+            <li>Contato</li>
+            <li>Tecnologias</li>
+            <li>Formação</li>
+        </ul>
+    </nav>
+    
+    
+    
+    </>
+)
+
+}
+
+export default header 
